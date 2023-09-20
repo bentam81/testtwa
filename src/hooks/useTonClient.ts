cc4188188c735a4dbdded56808ef81fcc634d6df
Ben Tam <ben.tam@busywhale.com>
2023-09-20 19:51:54 +0800

    use toncenter API

--- src/hooks/useTonClient.ts
+++ src/hooks/useTonClient.ts
@@ -11,11 +11,16 @@ export function useTonClient() {
   return {
     client: useAsyncInitialize(async () => {
       if (!network) return;
-      return new TonClient({
-        endpoint: await getHttpEndpoint({
-          network: network === CHAIN.MAINNET ? "mainnet" : "testnet",
-        }),
-      });
+
+      // return new TonClient({
+      //   endpoint: await getHttpEndpoint({
+      //     network: network === CHAIN.MAINNET ? "mainnet" : "testnet",
+      //   }),
+      // });
+
+      const endpoint = network === CHAIN.MAINNET ? "https://testnet.toncenter.com/api/v2/jsonRPC" : "https://toncenter.com/api/v2/jsonRPC"
+      const apiKey = network === CHAIN.MAINNET ? "65b36c975f91d756e221ec46e2983d1139f1037c946f288ee31e538620f2e581" : "4d3caa4fa0d225510539dd2a1e4d69cf72788247d1a4890c8d0eae62e413f15b"
+      return new TonClient({ endpoint, apiKey });
     }, [network]),
   };
 }
