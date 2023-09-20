a65770ba123bec53934763278b0fa99ffe8d57d4
Ben Tam <ben.tam@busywhale.com>
2023-09-20 19:55:49 +0800

    fix toncenter API url

--- src/hooks/useTonClient.ts
+++ src/hooks/useTonClient.ts
@@ -18,7 +18,7 @@ export function useTonClient() {
       //   }),
       // });
 
-      const endpoint = network === CHAIN.MAINNET ? "https://testnet.toncenter.com/api/v2/jsonRPC" : "https://toncenter.com/api/v2/jsonRPC"
+      const endpoint = network === CHAIN.MAINNET ? "https://toncenter.com/api/v2/jsonRPC" : "https://testnet.toncenter.com/api/v2/jsonRPC"
       const apiKey = network === CHAIN.MAINNET ? "65b36c975f91d756e221ec46e2983d1139f1037c946f288ee31e538620f2e581" : "4d3caa4fa0d225510539dd2a1e4d69cf72788247d1a4890c8d0eae62e413f15b"
       return new TonClient({ endpoint, apiKey });
     }, [network]),
